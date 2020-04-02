35942714680e5cd87d140a0baf4c611b35708f8d
Tez Kurmala <Tez.Kurmala@jda.com>
2020-04-02 15:47:24 +0530

    Enabling Hystrix and Refresh

--- src/main/java/com/boot/microservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/com/boot/microservices/limitsservice/LimitsConfigurationController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.boot.microservices.limitsservice.bean.LimitsConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class LimitsConfigurationController {
@@ -15,5 +16,17 @@ public class LimitsConfigurationController {
 	@GetMapping("/limits")
 	public LimitsConfiguration getLimitsConfiguration(){
 		return new LimitsConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+	
+	@GetMapping("/limits-hystrix")
+	@HystrixCommand(fallbackMethod = "getDefaultLimits")
+	public LimitsConfiguration getLimitsConfigurationFaultTolerant(){
+		throw new RuntimeException("Unable to connect. "
+				+ "You should,still be good for now "
+				+ "as you have Hystrix configured to be fault tolerant!");
+	}
+	
+	private LimitsConfiguration getDefaultLimits() {
+		return new LimitsConfiguration(444, 42);
 	}
 }
